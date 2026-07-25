9086cde37ca3b486b3cc27e5920316d89d27ef75
Ayaan Ahmed shohan <45226581+aayaan911@users.noreply.github.com>
2026-07-25 06:40:21 +0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* RateBoard service worker: app shell offline, network-first for rates */
-var CACHE = "rateboard-v6";
+var CACHE = "rateboard-v7";
 var SHELL = ["./", "./index.html"];
 
 self.addEventListener("install", function(e){
