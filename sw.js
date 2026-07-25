8f1f48c840fa803b2a57cfa0d327d64d34a650d3
Ayaan Ahmed shohan <45226581+aayaan911@users.noreply.github.com>
2026-07-25 06:25:25 +0600

    PHP added, cleaner rate table, always-on drag reorder, Updated <date> label

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* RateBoard service worker: app shell offline, network-first for rates */
-var CACHE = "rateboard-v3";
+var CACHE = "rateboard-v5";
 var SHELL = ["./", "./index.html"];
 
 self.addEventListener("install", function(e){
